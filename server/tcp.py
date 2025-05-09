8959b1d58f3a8d080446a93c502687580d2bd44c
김진언 <rlawlsdjs4582@gmail.com>
2025-05-09 14:02:04 +0900

    Update tcp.py

--- server/tcp.py
+++ server/tcp.py
@@ -35,36 +35,37 @@ def handle_client(conn, addr):
                 print("[ERROR] Invalid JSON received.")
                 continue
 
-            sensor_type = message.get("sensor")
-            timestamp = message.get("time")
-            value = message.get("data")
-            rfid = message.get("rfid_tag")
+            purpose = message.get("purpose")
 
-            if rfid:
-                rfid_tag = rfid
-                print(f"[INFO] RFID Tag Received: {rfid_tag}")
+            if purpose == "verification":
+                rfid_uid = message.get("rfid_uid")
+                print(f"[INFO] RFID UID Received: {rfid_uid}")
                 verified = False
 
                 with get_db_connection() as conn_db:
                     with conn_db.cursor() as cursor:
-                        sql = "SELECT COUNT(*) FROM authorized_rfids WHERE tag_id = %s"
-                        cursor.execute(sql, (rfid_tag,))
+                        sql = "SELECT COUNT(*) FROM authorized_rfids WHERE uid = %s"
+                        cursor.execute(sql, (rfid_uid,))
                         result = cursor.fetchone()
                         if result and result[0] > 0:
                             verified = True
 
                 response = "PASS" if verified else "FAIL"
                 conn.sendall(response.encode() + b"\n")
-                print(f"[INFO] RFID Tag {rfid_tag} verification result sent: {response}")
+                print(f"[INFO] RFID UID {rfid_uid} verification result sent: {response}")
 
             else:
+                rfid_uid = message.get("rfid_uid")
+                shock = message.get("shock")
+                temp = message.get("temperature")
+
                 # stores data to db
-                value_to_store = json.dumps(value) if isinstance(value, list) else value
+                # value_to_store = json.dumps(value) if isinstance(value, list) else value
                 with get_db_connection() as conn_db:
                     with conn_db.cursor() as cursor:
-                        sql = "INSERT INTO test (sensor, realtime, data) VALUES (%s, %s, %s)"
-                        cursor.execute(sql, (sensor_type, timestamp, value_to_store))
-                print(f"[INFO] Sensor data from {sensor_type} stored in DB.")
+                        sql = "INSERT INTO test (uid, shock, temperature) VALUES (%s, %s, %s)"
+                        cursor.execute(sql, (rfid_uid, shock, temp))
+                print(f"[INFO] Sensor data successfully stored in DB.")
 
     except Exception as e:
         print(f"[ERROR] Unexpected error: {e}")
