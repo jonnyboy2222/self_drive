7690b1b60aeba3d59f4845a88eff05b9811860f4
이동훈 <98556022+donghunl33@users.noreply.github.com>
2025-05-09 13:46:49 +0900

    UPDATE

--- vision/motor_control/motor_control.ino
+++ vision/motor_control/motor_control.ino
@@ -1,63 +1,82 @@
 #include <Servo.h>
+#include <SoftwareSerial.h>
+
+#define BT_RXD 2
+#define BT_TXD 3
+SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 
 class MotorControl {
   private:
-    int in1, in2, in3, in4;
+    int in1, in2, in3, in4, in5, in6;
 
   public:
-    MotorControl(int _in1, int _in2, int _in3, int _in4) {
+    MotorControl(int _in1, int _in2, int _in3, int _in4, int _in5, int _in6) {
       in1 = _in1;
       in2 = _in2;
       in3 = _in3;
       in4 = _in4;
+      in5 = _in5;
+      in6 = _in6;
 
       pinMode(in1, OUTPUT);
       pinMode(in2, OUTPUT);
+      pinMode(in3, OUTPUT);
       pinMode(in4, OUTPUT);
+      pinMode(in5, OUTPUT);
+      pinMode(in6, OUTPUT);
     }
 
     void moveForward(int speed = 150) {
-      analogWrite(in1, speed);
-      analogWrite(in2, 0);
-      analogWrite(in3, speed);
-      analogWrite(in4, 0);
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      analogWrite(in5, speed);
+      analogWrite(in6, speed);
     }
 
     void moveBackward(int speed = 150) {
-      analogWrite(in1, 0);
-      analogWrite(in2, speed);
-      analogWrite(in3, 0);
-      analogWrite(in4, speed);
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      analogWrite(in5, speed);
+      analogWrite(in6, speed);
     }
 
     void stopMotors() {
-      analogWrite(in1, 0);
-      analogWrite(in2, 0);
-      analogWrite(in3, 0);
-      analogWrite(in4, 0);
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+      analogWrite(in5, 0);
+      analogWrite(in6, 0);
     }
 };
 
 // 핀 번호 상수
-const int IN1 = 5;
-const int IN2 = 6;
-const int IN3 = 10;
-const int IN4 = 11;
+const int IN1 = 12;
+const int IN2 = 13;
+const int IN3 = 7;
+const int IN4 = 8;
+const int IN5 = 5;
+const int IN6 = 6;
 
-MotorControl motor(IN1, IN2, IN3, IN4); // 모터 컨트롤러 객체 생성
+
+MotorControl motor(IN1, IN2, IN3, IN4, IN5, IN6); // 모터 컨트롤러 객체 생성
 Servo steering;
 
 String input = "";
 
 void setup() {
-  Serial.begin(9600);
-  steering.attach(3); // 서보모터 핀
+  bluetooth.begin(9600);  
+  steering.attach(11); // 서보모터 핀
   steering.write(90); // 초기 각도
 }
 
 void loop() {
-  while (Serial.available()) {
-    char c = Serial.read();
+  while (bluetooth.available()) {
+    char c = bluetooth.read();
     if (c == '\n') {
       handleCommand(input);
       input = "";
--- vision/shock/shock.ino
+++ vision/shock/shock.ino
@@ -1,8 +1,13 @@
 const int LED = 13;
 const int CENSOR = A0;
+int shock_cnt = 0;
+int detect = 0;
 
 unsigned long prevMillis = millis();
-const long delayTime = 5000;
+unsigned long prevMillis2 = millis();
+
+const long delayTime = 1000;
+const long delayTime2 = 500;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,19 +18,46 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  double per = shock_cnt / detect * 100;
+
   unsigned long curMillis = millis();
+  unsigned long curMillis2 = millis();
   int tilt = digitalRead(CENSOR);
 
+  if (curMillis2 - prevMillis2 >= delayTime2) {
+    int tilt = digitalRead(CENSOR);
+    detect += 1;
+
+    if (tilt == 0) {
+      digitalWrite(LED, LOW);
+    }
+
+    else {
+      digitalWrite(LED, HIGH);
+      shock_cnt += 1;
+    }
+
+    prevMillis2 = curMillis2;
+  }
+
   if (curMillis - prevMillis >= delayTime) {
-    Serial.println(tilt);
+    Serial.println(shock_cnt);
+    Serial.println(detect);
+    Serial.println(per);
+    Serial.println("\n");
+
+    prevMillis = curMillis;
   }
 
-  if (tilt == 0) {
-    digitalWrite(LED, LOW);
-  }
+  
+  // Serial.println(tilt);
+  // if (tilt == 0) {
+  // digitalWrite(LED, LOW);
+  // }
 
-  else {
-    digitalWrite(LED, HIGH);
-  }
+  // else {
+  //   digitalWrite(LED, HIGH);
+  //   shock_cnt += 1;
+  // }
 
 }
