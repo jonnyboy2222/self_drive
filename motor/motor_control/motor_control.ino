694fae855246a636e12e53e4018115e02a3b1a48
이동훈 <98556022+donghunl33@users.noreply.github.com>
2025-05-14 13:40:07 +0900

    motor update

--- motor/motor_control/motor_control.ino
+++ motor/motor_control/motor_control.ino
@@ -1,19 +1,16 @@
 class MotorControl {
   private:
-    int in1, in2, in3, in4, in5, in6, in7, in8, in9, in10;
+    int in1, in2, in3, in4, in5, in6;
 
   public:
-    MotorControl(int _in1, int _in2, int _in3, int _in4, int _in5, int _in6, int _in7, int _in8, int _in9, int _in10) {
+    MotorControl(int _in1, int _in2, int _in3, int _in4, int _in5, int _in6) {
       in1 = _in1;
       in2 = _in2;
       in3 = _in3;
       in4 = _in4;
       in5 = _in5;
       in6 = _in6;
-      in7 = _in7;
-      in8 = _in8;
-      in9 = _in9;
-      in10 = _in10;
+
 
       pinMode(in1, OUTPUT);
       pinMode(in2, OUTPUT);
@@ -21,10 +18,6 @@ class MotorControl {
       pinMode(in4, OUTPUT);
       pinMode(in5, OUTPUT);
       pinMode(in6, OUTPUT);
-      pinMode(in7, OUTPUT);
-      pinMode(in8, OUTPUT);
-      pinMode(in9, OUTPUT);
-      pinMode(in10, OUTPUT);
     }
 
     void moveForward(int speed = 150) {
@@ -34,10 +27,6 @@ class MotorControl {
       digitalWrite(in4, LOW);
       analogWrite(in5, speed);
       analogWrite(in6, speed);
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, HIGH);
-      digitalWrite(in4, LOW);
     }
 
     void moveBackward(int speed = 150) {
@@ -47,10 +36,6 @@ class MotorControl {
       digitalWrite(in4, HIGH);
       analogWrite(in5, speed);
       analogWrite(in6, speed);
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
     }
 
     void turnLeft(int speed = 150) {
@@ -60,10 +45,6 @@ class MotorControl {
       digitalWrite(in4, LOW);
       analogWrite(in5, speed);
       analogWrite(in6, speed);
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      digitalWrite(in3, HIGH);
-      digitalWrite(in4, LOW);
     }
 
     void turnRight(int speed = 150) {
@@ -73,10 +54,6 @@ class MotorControl {
       digitalWrite(in4, HIGH);
       analogWrite(in5, speed);
       analogWrite(in6, speed);
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
     }
 
 
@@ -87,10 +64,6 @@ class MotorControl {
       digitalWrite(in4, LOW);
       analogWrite(in5, 0);
       analogWrite(in6, 0);
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, LOW);
     }
 };
 
@@ -101,13 +74,9 @@ const int IN3 = 6;
 const int IN4 = 7;
 const int IN5 = 3;
 const int IN6 = 9;
-const int IN7 = 8;
-const int IN8 = 10;
-const int IN9 = 11;
-const int IN10 = 12;
 
 
-MotorControl motor(IN1, IN2, IN3, IN4, IN5, IN6, IN7, IN8, IN9, IN10); // 모터 컨트롤러 객체 생성
+MotorControl motor(IN1, IN2, IN3, IN4, IN5, IN6); // 모터 컨트롤러 객체 생성
 int speed = 150;
 
 String input = "";
